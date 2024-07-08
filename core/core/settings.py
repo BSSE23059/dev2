db3fa407cf00508178a770af582cb9403a049354
TOSHIBA <bsse23059@itu.edu.pk>
2024-07-08 21:27:21 +0500

    L

--- core/core/settings.py
+++ core/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_voh3h4%89vr2jw9li+n2!*z%wb(o&^9a8j=^dao44bu4%ujv=
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app']
+ALLOWED_HOSTS = ['.vercel.app','127.0.0.1']
 
 
 # Application definition
